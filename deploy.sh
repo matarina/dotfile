6e3ad9a58ca55457504912417462f0435a5c231b
matarina <83324182+matarina@users.noreply.github.com>
2024-09-10 03:41:24 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,104 +1,47 @@
 #!/bin/bash
 
-sudo apt update
-touch /home/ma/.xinit
-echo "exec bspwm" >> /home/ma/.xinit
-# List of packages to check and install
-packages=(
-    xorg
-    xinit
-    vim
-    sudo
-    curl
-    bspwm
-    sxhkd
-    picom
-    rofi
-    ibus
-    fonts-wqy-microhei
-    fonts-noto-cjk
-    ibus-rime
-    pulseaudio
-    pavucontrol
-    paprefs
-    tmux
-    dunst
-    feh
-    kitty
-    neovim
-    thunar
-    firefox-esr
-    htop
-    libreoffice
-)
+# Exit immediately if a command exits with a non-zero status
+set -e
 
-# Update package list
-echo "Updating package list..."
+# Update system
+echo "Updating system..."
+sudo apt update && sudo apt upgrade -y
 
-# Function to install polybar from source
+# Install dependencies
+echo "Installing dependencies..."
+sudo apt install -y build-essential git xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3lock xss-lock
 
-# Iterate over the list of packages
-for package in "${packages[@]}"; do
-    if dpkg -l | grep -q "^ii  $package "; then
-        echo "$package is already installed."
-    else
-        echo "Installing $package..."
-        sudo apt install -y "$package"
-                ;;
-    fi
-done
-
-echo "All packages checked and processed."
-
-#install polybar from source 
-sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev \
-    libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto \
-    libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
-git clone --recursive https://github.com/polybar/polybar
-cd polybar && mkdir build && cd build
-cmake ..
-make -j4
+# Clone, compile and install bspwm
+echo "Installing bspwm..."
+git clone https://github.com/baskerville/bspwm.git
+cd bspwm
+make
 sudo make install
-cd "$HOME/dotfile"
+cd ..
 
-#install neovim from source 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+# Clone, compile and install sxhkd
+echo "Installing sxhkd..."
+git clone https://github.com/baskerville/sxhkd.git
+cd sxhkd
+make
+sudo make install
+cd ..
 
-cat << 'EOF' >> ~/.bashrc
-export PATH="$PATH:/opt/nvim-linux64/bin"
-alias vi='nvim'
-alias proxyon='export http_proxy=http://127.0.0.1:7897;export https_proxy=http://127.0.0.1:7897'
-EOF
-source ~/.bashrc
-
-
-
-# download hack nerd-fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/Hack.zip
-unzip Hack.zip -d $HOME/.local/share/fonts
-fc-cache -fv
-rm Hack.zip
-
-wget https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.10_240715_amd64_01.deb
-sudo dpkg -i QQ_3.2.10_240715_amd64_01.deb
-rm QQ_3.2.10_240715_amd64_01.deb 
-wget https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v1.7.3/clash-verge_1.7.3_amd64.deb
-sudo dpkg -i clash-verge_1.7.3_amd64.deb
-rm clash-verge_1.7.3_amd64.deb
+# Create config directories
+echo "Creating config directories..."
+mkdir -p ~/.config/{bspwm,sxhkd}
 
 
-# wechat install
-wget -c -O atzlinux-v12-archive-keyring_lastest_all.deb https://www.atzlinux.com/atzlinux/pool/main/a/atzlinux-archive-keyring/atzlinux-v12-archive-keyring_lastest_all.deb
-sudo apt -y install ./atzlinux-v12-archive-keyring_lastest_all.deb
-sudo apt update
-sudo cp /etc/lsb-release /etc/lsb-release.Ubuntu
-sudo apt -y install com.tencent.wechat
-sudo apt -y install electronic-wechat-icons-atzlinux
-sudo cp /etc/lsb-release /etc/lsb-release.wechat
+
+# Make bspwmrc executable
+chmod +x ~/.config/bspwm/bspwmrc
 
 
-mkdir /home/ma/.config
-mv ./* /home/ma/.config
 
+# Create .xinitrc file
+echo "Creating .xinitrc..."
+echo "exec bspwm" > ~/.xinitrc
+
+# Cleanup
+echo "Cleaning up..."
+rm -rf bspwm sxhkd
