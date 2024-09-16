9b7a522fe7530c010df573ffaa482f7aa57463a0
matarina <maxiaowei2020@foxmail.com>
2024-09-16 06:04:24 +0000

    latest

--- deploy.sh
+++ deploy.sh
@@ -2,32 +2,79 @@
 
 # Modified options here
 clash_url="https://huahe.link/link/ug1nhgZZ0d7Wpzua?clash=2"
-
+myhome="/home/ma"
 set -e
+
+# Function to check if the system is Ubuntu
+is_ubuntu() {
+    if [ -f /etc/lsb-release ]; then
+        if grep -q "DISTRIB_ID=Ubuntu" /etc/lsb-release; then
+            return 0
+        fi
+    fi
+    return 1
+}
+
+# Main script
+if is_ubuntu; then
+    echo "Ubuntu detected. Proceeding with operations..."
+
+    # Remove snapd and related directories
+    sudo rm -rf /var/cache/snapd/
+    rm -fr ~/snap
+    sudo apt autoremove --purge snapd -y
+
+    # Hold snapd package to prevent reinstallation
+    sudo apt-mark hold snapd
+
+    # Add Mozilla Team PPA
+    sudo add-apt-repository ppa:mozillateam/ppa -y
+
+    # Create preferences file for Firefox
+    echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+
+Package: firefox
+Pin: version 1:1snap*
+Pin-Priority: -1
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+
+    echo "Operations completed successfully."
+else
+    echo "This script is intended for Ubuntu systems only."
+    exit 1
+fi
+
+
 
 # Update system and install dependencies
 echo "Updating system and installing dependencies..."
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y build-essential xcb libxcb-xinerama0-dev libxcb-util0-dev libxcb-icccm4-dev libxcb-randr0-dev \
-    libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev xinit eza git htop rofi stalonetray feh scrot firefox ibus ibus-rime redshift
+sudo apt install -y build-essential xcb libx11-xcb-dev libxcb-xinerama0-dev \
+		    libxcb-util0-dev libxcb-icccm4-dev libxcb-randr0-dev libxft-dev \
+      		    libxcb-ewmh-dev  x11-xserver-utils libxcb-keysyms1-dev libxcb-shape0-dev \
+    		    xinit vim git htop rofi stalonetray feh scrot ibus ibus-rime redshift \
+    		    neovim kitty bc unzip
 
 # Deploy clash VPN
 echo "Deploying clash VPN"
-curl -LO https://raw.githubusercontent.com/matarina/dotfile/master/clash_terminal.tar.gz
-sudo tar -C "$HOME/.local/" -xvf clash_terminal.tar.gz && rm clash_terminal.tar.gz
-sudo bash "$HOME/.local/clash_terminal/start.sh" -u "$clash_url"
-source /etc/profile.d/clash.sh
+curl -LO https://gitee.com/petrioma/clash/raw/master/clash_terminal.tar.gz
+mkdir -p $myhome/.local
+tar -C "$myhome/.local/" -xvf clash_terminal.tar.gz && rm clash_terminal.tar.gz
+sudo bash "$myhome/.local/clash_terminal/start.sh" -u "$clash_url"
+. /etc/profile.d/clash.sh
 proxy_on
-
-# Test connection
-if curl -s www.google.com > /dev/null; then
-    echo "curl www.google.com produced output successfully"
-else
-    echo "Error: curl www.google.com failed" >&2
-    exit 1
-fi
-
 echo "Proxy config done"
+
+export http_proxy=http://127.0.0.1:7890
+export https_proxy=http://127.0.0.1:7890
+export HTTP_PROXY=http://127.0.0.1:7890
+export HTTPS_PROXY=http://127.0.0.1:7890
+
+# Install fireofx with proxy
+sudo apt -o Acquire::https::Proxy="http://127.0.0.1:7890" install firefox
 
 # Install bspwm and sxhkd
 echo "Installing bspwm and sxhkd..."
@@ -38,36 +85,61 @@ for repo in bspwm sxhkd; do
 done
 
 # Create .xinitrc file
-echo "exec bspwm" > "$HOME/.xinitrc"
+echo "exec bspwm" > "$myhome/.xinitrc"
 
-# Install neovim
-echo "Installing neovim..."
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf "$HOME/.local/nvim"
-sudo tar -C "$HOME/.local/" -xzf nvim-linux64.tar.gz && rm nvim-linux64.tar.gz
-
-# Install kitty
-echo "Installing kitty..."
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 # Install lemonbar-xft
 echo "Installing lemonbar-xft..."
-git clone https://github.com/drscream/lemonbar-xft.git "$HOME/.local/lemonbar-xft"
-(cd "$HOME/.local/lemonbar-xft" && make && sudo make install)
+git clone https://github.com/drscream/lemonbar-xft.git "$myhome/.local/lemonbar-xft"
+(cd "$myhome/.local/lemonbar-xft" && make && sudo make install)
 
-# Move configuration files
-mv "$HOME/dotfile/.stalonetrayrc" "$HOME/dotfile/wall.png" "$HOME/.config/"
 
 # Update .bashrc
-cat << 'EOF' >> "$HOME/.bashrc"
-export PATH="$PATH:$HOME/.local/nvim-linux64/bin:$HOME/.local/kitty.app/bin:$HOME/.local/lemonbar"
+
+# Update .bashrc
+content="
+function proxy_on() {
+        export http_proxy=http://127.0.0.1:7890
+        export https_proxy=http://127.0.0.1:7890
+        export no_proxy=127.0.0.1,localhost
+        export HTTP_PROXY=http://127.0.0.1:7890
+        export HTTPS_PROXY=http://127.0.0.1:7890
+        export NO_PROXY=127.0.0.1,localhost
+        echo -e \"proxy opened\"
+}
+
+function proxy_off(){
+        unset http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY
+        echo -e \"proxy closed\"
+}
+
 export GTK_IM_MODULE=ibus
 export QT_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
-EOF
+
+export PATH=\"\$PATH:/home/ma/.local/lemonbar\"
+"
+
+# Check if content already exists in .bashrc
+if ! grep -qF "function proxy_on()" "/home/ma/.bashrc"; then
+    echo "$content" >> "/home/ma/.bashrc"
+    echo "Vars added to .bashrc"
+else
+    echo "Vars already exists in .bashrc"
+fi
+
+#Install fonts
+mkdir -p $myhome/.local/share/fonts/Hack
+curl -L -o Hack.zip   --progress-bar  https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
+unzip Hack.zip -d $myhome/.local/share/fonts/Hack
+fc-cache -f
 
 
+#load config file
 git clone https://github.com/matarina/dotfile.git
-mv dotfile/* $HOME/.config
+mkdir -p $myhome/.config
+mv dotfile/* $myhome/.config
+
 
 echo "Setup complete!"
+
